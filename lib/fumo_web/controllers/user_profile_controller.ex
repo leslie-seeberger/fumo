21fb208c6b1853b6842991d974acc0dda3729b94
Leslie Seeberger <leslie.c.seeberger@gmail.com>
2021-10-16 10:32:48 -0500

    Update templates

--- lib/fumo_web/controllers/user_profile_controller.ex
+++ lib/fumo_web/controllers/user_profile_controller.ex
@@ -3,22 +3,17 @@ defmodule FumoWeb.UserProfileController do
 
   alias Fumo.Profiles
   alias Fumo.Profiles.UserProfile
+  alias Fumo.FlashCards
 
   def show(conn, %{"username" => username}) do
     case Profiles.get_user_profile_by_username(username) do
-      user_profile when is_struct(user_profile, UserProfile) -> render(conn, "show.html", user_profile: user_profile)
+      user_profile when is_struct(user_profile, UserProfile) ->
+        decks = FlashCards.list_user_decks(user_profile.user_id)
+        render(conn, "show.html", user_profile: user_profile, decks: decks)
       _ -> conn
         |> put_flash(:error, "Profile not found")
         |> redirect(to: "/")
     end
-
-    # if user_profile = Profiles.get_user_profile_by_username(username) do
-    #   render(conn, "show.html", user_profile: user_profile)
-    # else
-    #   conn
-    #     |> put_flash(:error, "Profile not found")
-    #     |> redirect(to: "/")
-    # end
   end
 
 end
