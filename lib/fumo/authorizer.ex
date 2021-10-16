d84d661e3a6738612cc373a8e126a764d27ac5a0
Leslie Seeberger <leslie.c.seeberger@gmail.com>
2021-10-16 10:22:07 -0500

    Generalize can_manage? to work with multiple schemas

--- lib/fumo/authorizer.ex
+++ lib/fumo/authorizer.ex
@@ -1,9 +1,7 @@
 defmodule Fumo.Authorizer do
   alias Fumo.Accounts.User
-  alias Fumo.FlashCards.Deck
-  def can_manage?(%User{id: user_id}, %Deck{user_id: deck_user_id}) do
-    user_id == deck_user_id
-  end
+
+  def can_manage?(%User{id: user_id}, %{user_id: resource_user_id}), do: user_id == resource_user_id
 
   def can_manage?(user, resource) when is_nil(user) or is_nil(resource), do: false
 end
